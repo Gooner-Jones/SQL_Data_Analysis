d77a41253186577e7f6bcc44f4b41b9b11482c9c
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 13:04:14 +0200

    Update 06 ranking_analysis.sql

--- scripts/Exploratory Data Analysis/06 ranking_analysis.sql
+++ scripts/Exploratory Data Analysis/06 ranking_analysis.sql
@@ -8,48 +8,48 @@
 
 -- Which 5 products generate the highest revenue?
 SELECT TOP 5
-	   p.product_name,
-	   SUM(f.sales_amount) 'Total Revenue'
+       p.product_name,
+       SUM(f.sales_amount) 'Total Revenue'
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
+ON 	  p.product_key = f.product_key
 GROUP BY product_name
 ORDER BY 2 DESC 
 
 -- Which 5 products generate the least revenue?
 SELECT TOP 5
-	   p.product_name,
-	   SUM(f.sales_amount) 'Total Revenue'
+       p.product_name,
+       SUM(f.sales_amount) 'Total Revenue'
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
+ON 	  p.product_key = f.product_key
 GROUP BY product_name
 ORDER BY 2
 
 -- Find the top 10 customers who have generated the highest revenue
 SELECT TOP 10
-	   c.customer_key,
-	   c.first_name,
-	   c.last_name,
-	   SUM(f.sales_amount) 'Total Revenue'
+       c.customer_key,
+       c.first_name,
+       c.last_name,
+       SUM(f.sales_amount) 'Total Revenue'
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
-ON	c.customer_key = f.customer_key
+ON	  c.customer_key = f.customer_key
 GROUP BY c.customer_key,
-		 c.first_name,
-		 c.last_name
+	 c.first_name,
+	 c.last_name
 ORDER BY 4 DESC
 
 -- The 3 customers with the fewest orders placed
 SELECT TOP 3
-	   c.customer_key,
-	   c.first_name,
-	   c.last_name,
-	   COUNT(DISTINCT order_date) 'Total Orders'
+       c.customer_key,
+       c.first_name,
+       c.last_name,
+       COUNT(DISTINCT order_date) 'Total Orders'
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
-ON	c.customer_key = f.customer_key
+ON	  c.customer_key = f.customer_key
 GROUP BY c.customer_key,
-		 c.first_name,
-		 c.last_name
+	 c.first_name,
+	 c.last_name
 ORDER BY 4
