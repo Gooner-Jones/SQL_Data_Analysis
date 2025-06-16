fd66090bb0a9b968a5ba341a3d0eabf10df6bfd3
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 13:24:08 +0200

    Update 01 change_over_time_analysis.sql

--- scripts/Advanced Analytics/01 change_over_time_analysis.sql
+++ scripts/Advanced Analytics/01 change_over_time_analysis.sql
@@ -12,7 +12,7 @@
 
 -- Quick Date Functions
 SELECT YEAR(order_date) 'Order Year',
-	   MONTH(order_date) 'Oder Month',
+       MONTH(order_date) 'Oder Month',
        SUM(sales_amount) 'Total Sales',
        COUNT(DISTINCT customer_key) 'Total Customers',
        SUM(quantity) AS total_quantity
@@ -23,7 +23,7 @@ ORDER BY YEAR(order_date), MONTH(order_date);
 
 -- DATETRUNC()
 SELECT DATETRUNC(month, order_date) 'Order Date',
-	   SUM(sales_amount) 'Total Sales',
+       SUM(sales_amount) 'Total Sales',
        COUNT(DISTINCT customer_key) 'Total Customers',
        SUM(quantity) AS total_quantity
 FROM gold.fact_sales
@@ -43,7 +43,7 @@ ORDER BY FORMAT(order_date, 'yyyy-MMM');
 
 -- How many new customers were added each year
 SELECT DATETRUNC(YEAR, create_date) 'Create Year',
-	   COUNT(customer_key) 'Total Customers'
+       COUNT(customer_key) 'Total Customers'
 FROM gold.dim_customers
 GROUP BY DATETRUNC(YEAR, create_date)
 ORDER BY 1;
