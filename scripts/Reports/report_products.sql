40000b405850dfa6d3b7b199bf9239c576ca5972
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 14:23:10 +0200

    Update report_products.sql

--- scripts/Reports/report_products.sql
+++ scripts/Reports/report_products.sql
@@ -24,7 +24,7 @@ Highlights:
 -- Create Report: gold.report_products
 -- ==========================================================================================================================
 IF OBJECT_ID('gold.report_products', 'V') IS NOT NULL
- DROP VIEW gold.report_products;
+DROP VIEW gold.report_products;
 GO
 
 CREATE VIEW gold.report_products AS
@@ -35,7 +35,7 @@ WITH base_query AS (
 ---------------------------------------------------------------------------*/
     SELECT f.order_number,
            f.order_date,
-		   f.customer_key,
+	   f.customer_key,
            f.sales_amount,
            f.quantity,
            p.product_key,
@@ -45,7 +45,7 @@ WITH base_query AS (
            p.cost
     FROM gold.fact_sales f
     LEFT JOIN gold.dim_products p
-    ON		  f.product_key = p.product_key
+    ON	      f.product_key = p.product_key
     WHERE order_date IS NOT NULL  -- only consider valid sales dates
 ),
 
@@ -61,47 +61,47 @@ SELECT product_key,
        DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
        MAX(order_date) AS last_sale_date,
        COUNT(DISTINCT order_number) AS total_orders,
-	   COUNT(DISTINCT customer_key) AS total_customers,
+       COUNT(DISTINCT customer_key) AS total_customers,
        SUM(sales_amount) AS total_sales,
        SUM(quantity) AS total_quantity,
-	   ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+       ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
 FROM base_query
 
 GROUP BY product_key,
-		 product_name,
-		 category,
-		 subcategory,
-		 cost
+	 product_name,
+	 category,
+	 subcategory,
+	 cost
 )
 
 /*---------------------------------------------------------------------------
   3) Final Query: Combines all product results into one output
 ---------------------------------------------------------------------------*/
 SELECT product_key,
-	   product_name,
-	   category,
-	   subcategory,
-	   cost,
-	   last_sale_date,
-	   DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
-	   CASE WHEN total_sales > 50000 THEN 'High-Performer'
-		    WHEN total_sales >= 10000 THEN 'Mid-Range'
-		    ELSE 'Low-Performer'
-	   END AS product_segment,
-	   lifespan,
-	   total_orders,
-	   total_sales,
-	   total_quantity,
-	   total_customers,
-	   avg_selling_price,
-	-- Average Order Revenue (AOR)
-	   CASE WHEN total_orders = 0 THEN 0
-			ELSE total_sales / total_orders
-	   END AS avg_order_revenue,
+       product_name,
+       category,
+       subcategory,
+       cost,
+       last_sale_date,
+       DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
+       CASE WHEN total_sales > 50000 THEN 'High-Performer'
+	    WHEN total_sales >= 10000 THEN 'Mid-Range'
+	    ELSE 'Low-Performer'
+       END AS product_segment,
+       lifespan,
+       total_orders,
+       total_sales,
+       total_quantity,
+       total_customers,
+       avg_selling_price,
+-- Average Order Revenue (AOR)
+       CASE WHEN total_orders = 0 THEN 0
+	    ELSE total_sales / total_orders
+       END AS avg_order_revenue,
 
-	-- Average Monthly Revenue
-	   CASE WHEN lifespan = 0 THEN total_sales
-			ELSE total_sales / lifespan
-	   END AS avg_monthly_revenue
+-- Average Monthly Revenue
+       CASE WHEN lifespan = 0 THEN total_sales
+	    ELSE total_sales / lifespan
+       END AS avg_monthly_revenue
 
 FROM product_aggregations 
