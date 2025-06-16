0406762f1753b4f751d316c38e248b6c7572ebc4
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 12:29:06 +0200

    Update 02 dimensions_exploreation.sql

--- scripts/02 dimensions_exploreation.sql
+++ scripts/02 dimensions_exploreation.sql
@@ -6,7 +6,7 @@
 /* Identifying the unique values (or category) in each dimension.
    Recognizing how data might be grouped or segmented which useful for later analysis
 */
--- Explore all Customers country of origin
+
 
 -- Retrieve a list of unique countries from which customers originate
 SELECT DISTINCT country 
@@ -15,7 +15,7 @@ ORDER BY country;
 
 -- Retrieve a list of unique categories, subcategories, and products
 SELECT DISTINCT category, 
-				        subcategory, 
-				        product_name 
+		subcategory, 
+		product_name 
 FROM gold.dim_products
 ORDER BY category, subcategory, product_name;
