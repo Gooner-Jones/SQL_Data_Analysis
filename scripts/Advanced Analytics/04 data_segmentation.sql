e3a94675d0d22993a1ea517835c83cdda2914d91
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 13:44:56 +0200

    Update 04 data_segmentation.sql

--- scripts/Advanced Analytics/04 data_segmentation.sql
+++ scripts/Advanced Analytics/04 data_segmentation.sql
@@ -13,14 +13,14 @@ WITH product_segments AS (
 		   product_name,
 		   cost,
 		   CASE WHEN cost < 100 THEN 'Below 100'
-				WHEN cost BETWEEN 100 AND 500 THEN '100-500'
-				WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
-				ELSE 'Above 1000'
+			WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+			WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+			ELSE 'Above 1000'
 		   END AS cost_range
     FROM gold.dim_products
 )
 SELECT cost_range,
-	   COUNT(product_key) 'Total Products'
+       COUNT(product_key) 'Total Products'
 FROM product_segments
 GROUP BY cost_range
 ORDER BY total_products DESC;
@@ -33,22 +33,22 @@ And find the total number of customers by each group
 */
 WITH customer_spending AS (
 	SELECT c.customer_key,
-		   SUM(f.sales_amount) 'Total Spending',
-		   MIN(order_date) 'First Order',
-		   MAX(order_date) 'Last Order',
-		   DATEDIFF(month, MIN(order_date), MAX(order_date)) 'Lifespan'
+	       SUM(f.sales_amount) 'Total Spending',
+	       MIN(order_date) 'First Order',
+	       MAX(order_date) 'Last Order',
+	       DATEDIFF(month, MIN(order_date), MAX(order_date)) 'Lifespan'
     FROM gold.fact_sales f
     LEFT JOIN gold.dim_customers c
-    ON		  f.customer_key = c.customer_key
+    ON	      f.customer_key = c.customer_key
     GROUP BY c.customer_key
 )
 SELECT customer_segment,
-	   COUNT(customer_key) AS total_customers
+       COUNT(customer_key) AS total_customers
 FROM (SELECT customer_key,
-			CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
-				 WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
-				 ELSE 'New'
-			END AS customer_segment
+	     CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+		  WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+		  ELSE 'New'
+	     END AS customer_segment
       FROM customer_spending) AS segmented_customers
 GROUP BY customer_segment
 ORDER BY total_customers DESC;
