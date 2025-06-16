271ca44180e3c9550f864dba9cf587bd5cc24de8
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 13:28:32 +0200

    Update 02 cumulative_analysis.sql

--- scripts/Advanced Analytics/02 cumulative_analysis.sql
+++ scripts/Advanced Analytics/02 cumulative_analysis.sql
@@ -9,13 +9,11 @@
 
 -- Calculate the total sales per Month/Year and the running total of sales over time
 SELECT order_date,
-	   total_sales,
-	   SUM(total_sales) OVER(ORDER BY order_date) 'Running Total'
-FROM 
-	(
-	SELECT DATETRUNC(YEAR, order_date) AS order_date,
-		   SUM(sales_amount) AS total_sales
-	FROM gold.fact_sales
-	WHERE order_date IS NOT NULL
-	GROUP BY DATETRUNC(YEAR, order_date)
-	)t
+       total_sales,
+       SUM(total_sales) OVER(ORDER BY order_date) 'Running Total'
+FROM (SELECT DATETRUNC(YEAR, order_date) AS order_date,
+	     SUM(sales_amount) AS total_sales
+      FROM gold.fact_sales
+      WHERE order_date IS NOT NULL
+      GROUP BY DATETRUNC(YEAR, order_date)
+      )t
