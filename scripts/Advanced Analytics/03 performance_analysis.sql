c9eff5f8972b430752033fe8eac45ba9a4b3bc95
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 13:33:52 +0200

    Update 03 performance_analysis.sql

--- scripts/Advanced Analytics/03 performance_analysis.sql
+++ scripts/Advanced Analytics/03 performance_analysis.sql
@@ -17,25 +17,25 @@ WITH yearly_product_sales AS
 		   SUM(f.sales_amount) current_sales
 	FROM gold.fact_sales f
 	LEFT JOIN gold.dim_products p
-	ON f.product_key = p.product_key
+	ON 	  f.product_key = p.product_key
 	WHERE f.order_date IS NOT NULL
 	GROUP BY YEAR(order_date), p.product_name
 )
 SELECT order_year,
-	   product_name,
-	   current_sales,
-	   AVG(current_sales) OVER(PARTITION BY product_name) 'Average Sales',
-	   current_sales - AVG(current_sales) OVER(PARTITION BY product_name) 'Diff Average',
-	   CASE WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) > 0 THEN 'Above Average'
-			WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) < 0 THEN 'Below Average'
-			ELSE 'Average'
-	   END AS 'Average Change',
-	   -- Year Over Year Change/ analysis
-	   LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) 'Previou Year Sales',
-	   current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) 'Diff Previous Year',
-	   CASE WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
-			WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
-			ELSE 'No change'
-	   END AS 'Previous Year Change'
+       product_name,
+       current_sales,
+       AVG(current_sales) OVER(PARTITION BY product_name) 'Average Sales',
+       current_sales - AVG(current_sales) OVER(PARTITION BY product_name) 'Diff Average',
+       CASE WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) > 0 THEN 'Above Average'
+	    WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) < 0 THEN 'Below Average'
+	    ELSE 'Average'
+       END AS 'Average Change',
+       -- Year Over Year Change/ analysis
+       LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) 'Previou Year Sales',
+       current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) 'Diff Previous Year',
+       CASE WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+	    WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+	    ELSE 'No change'
+       END AS 'Previous Year Change'
 FROM yearly_product_sales
 ORDER BY product_name, order_year
