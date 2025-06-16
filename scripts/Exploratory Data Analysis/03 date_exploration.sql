d617bc8c414c543a1252783b5d62f4314b46c231
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 12:36:56 +0200

    Update 03 date_exploration.sql

--- scripts/Exploratory Data Analysis/03 date_exploration.sql
+++ scripts/Exploratory Data Analysis/03 date_exploration.sql
@@ -9,14 +9,14 @@
 
 -- Determine the first and last order date and the total duration in months
 SELECT MIN(order_date) first_order_date,
-	     MAX(order_date) last_order_date,
-	     DATEDIFF(year, MIN(order_date), MAX(order_date)) order_range_year
+       MAX(order_date) last_order_date,
+       DATEDIFF(year, MIN(order_date), MAX(order_date)) order_range_year
 FROM gold.fact_sales
 
 
 -- Find the youngest and oldest customer based on birthdate
 SELECT MIN(birthdate) oldest_birthdate,
-	     DATEDIFF(YEAR,MIN(birthdate), GETDATE()) oldest_customer_age,
-	     MAX(birthdate) youngest_birthdate,
-	     DATEDIFF(YEAR, MAX(birthdate), GETDATE()) youngest_customer_age
+       DATEDIFF(YEAR,MIN(birthdate), GETDATE()) oldest_customer_age,
+       MAX(birthdate) youngest_birthdate,
+       DATEDIFF(YEAR, MAX(birthdate), GETDATE()) youngest_customer_age
 FROM gold.dim_customers
