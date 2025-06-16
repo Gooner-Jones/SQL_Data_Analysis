aefd3364907ac7d0e500bcd4fe3d48428b632a06
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 13:49:29 +0200

    Update 05 part_to_whole.sql

--- scripts/Advanced Analytics/05 part_to_whole.sql
+++ scripts/Advanced Analytics/05 part_to_whole.sql
@@ -11,15 +11,15 @@
 
 WITH category_sales AS (	
 	SELECT category,
-		   SUM(sales_amount) total_sales
+	       SUM(sales_amount) total_sales
 	FROM gold.fact_sales f
 	LEFT JOIN gold.dim_products p
-	ON f.product_key = p.product_key
+	ON 	  f.product_key = p.product_key
 	GROUP BY category
 )
 SELECT category,
-	   total_sales,
-	   SUM(total_sales) OVER() overall_sales,
-	   CONCAT(ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER())* 100, 2), '%') AS 'Percentage of total'
+       total_sales,
+       SUM(total_sales) OVER() overall_sales,
+       CONCAT(ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER())* 100, 2), '%') AS 'Percentage of total'
 FROM category_sales
 ORDER BY total_sales DESC
