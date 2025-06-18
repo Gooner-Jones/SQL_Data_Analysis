eb53457fb17e283f383ee4a7d8fe83850a30c2ef
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-18 20:42:52 +0200

    Update report_customers.sql

--- scripts/Reports/report_customers.sql
+++ scripts/Reports/report_customers.sql
@@ -7,7 +7,7 @@
 
 Highlights:
     1. Gathers essential fields such as names, ages, and transaction details.
-	2. Segments customers into categories (VIP, Regular, New) and age groups.
+    2. Segments customers into categories (VIP, Regular, New) and age groups.
     3. Aggregates customer-level metrics:
 	   - total orders
 	   - total sales
@@ -16,8 +16,8 @@ Highlights:
 	   - lifespan (in months)
     4. Calculates valuable KPIs:
 	    - recency (months since last order)
-		- average order value
-		- average monthly spend
+	    - average order value
+	    - average monthly spend
 */
 
 -- ==========================================================================================================================
